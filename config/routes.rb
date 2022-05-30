023ed9904d986e01b8df4bc2aa57e4748ec763cc
igor martins <igordavillamartins@outlook.com>
2022-05-30 14:42:08 -0300

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # root to: 'pages#home'
-  resources :tickets
+  resources :tickets do
+    resources :orders
+  end
   root to: 'tickets#index'
 end
