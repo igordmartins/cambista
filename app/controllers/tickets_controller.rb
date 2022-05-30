839ee192584ccd1ff5aa1e720604107d64dde376
thiago aragao <thiagohoaragao@gmail.com>
2022-05-30 15:13:55 -0300

    Added ticket controller

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -6,4 +6,27 @@ class TicketsController < ApplicationController
   def new
     @ticket = Ticket.new
   end
+
+  def create
+    @user = User.find(params[:user_id])
+    @ticket = Ticket.new(ticket_params)
+    @ticket.user = @user
+    if @ticket.save
+      redirect_to tickets_path(@ticket)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @ticket = Ticket.find(params[:id])
+    @ticket.destroy
+    redirect_to tickets_path(@ticket)
+  end
+
+  private
+
+  def ticket_params
+    params.require(:ticket).permit(:event_price, :event_name, :event_date, :user_id)
+  end
 end
