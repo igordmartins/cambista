fb6e2d20dd1d8aff8664247acb69ff36fb8d8e96
thiago aragao <thiagohoaragao@gmail.com>
2022-05-31 16:38:18 -0300

    Added Ticket Validations

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,8 @@
 class Ticket < ApplicationRecord
   belongs_to :user
   has_one :order, dependent: :destroy
+  validates :event_name, presence: true
+  validates :event_price, presence: true
+  validates :event_date, presence: true
+  validates :description, presence: true
 end
