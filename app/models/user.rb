96d80f329a8614d6456e3c06e82de1b81032a120
igor martins <igordavillamartins@outlook.com>
2022-05-31 16:45:23 -0300

    cloudinary waiting for forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :tickets, dependent: :destroy
   has_many :orders
+  has_one_attached :avatar
   validates :name, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
