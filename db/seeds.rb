f9b1f6324c8342b3462147941ae72795299e10d3
igor martins <igordavillamartins@outlook.com>
2022-05-31 12:04:02 -0300

    form changed, added description box

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,20 @@
-# user = User.create!(email: "teste@teste.com", password: "123123", name: "Dolores Suave Na Nave")
-# user2 = User.create!(email: "teste2@teste.com", password: "123123", name: "Josephino Da Silva")
+User.destroy_all
+Ticket.destroy_all
 
-# Ticket.create!(user: user, event_price: 99.99, event_name: "Evento de testes", event_date: DateTime.now)
-# Ticket.create!(user: user, event_price: 199.99, event_name: "Evento de testes2", event_date: DateTime.now)
-# Ticket.create!(user: user, event_price: 229.99, event_name: "Evento de testes3", event_date: DateTime.now)
-# Ticket.create!(user: user, event_price: 339.99, event_name: "Evento de testes4", event_date: DateTime.now)
-# Ticket.create!(user: user, event_price: 459.99, event_name: "Evento de testes5", event_date: DateTime.now)
+user = User.create!(email: "teste@teste.com", password: "123123", name: "Dolores Suave Na Nave")
+user2 = User.create!(email: "teste2@teste.com", password: "123123", name: "Josephino Da Silva")
 
-# Ticket.create!(user: user2, event_price: 99.99, event_name: "Evento de testes", event_date: DateTime.now)
-# Ticket.create!(user: user2, event_price: 199.99, event_name: "Evento de testes2", event_date: DateTime.now)
-# Ticket.create!(user: user2, event_price: 229.99, event_name: "Evento de testes3", event_date: DateTime.now)
-# Ticket.create!(user: user2, event_price: 339.99, event_name: "Evento de testes4", event_date: DateTime.now)
-# Ticket.create!(user: user2, event_price: 459.99, event_name: "Evento de testes5", event_date: DateTime.now)
+Ticket.create!(user: user, event_price: 99.99, event_name: "Evento de testes", event_date: DateTime.now)
+Ticket.create!(user: user, event_price: 199.99, event_name: "Evento de testes2", event_date: DateTime.now)
+Ticket.create!(user: user, event_price: 229.99, event_name: "Evento de testes3", event_date: DateTime.now)
+Ticket.create!(user: user, event_price: 339.99, event_name: "Evento de testes4", event_date: DateTime.now)
+Ticket.create!(user: user, event_price: 459.99, event_name: "Evento de testes5", event_date: DateTime.now)
+
+Ticket.create!(user: user2, event_price: 99.99, event_name: "Evento de testes", event_date: DateTime.now)
+Ticket.create!(user: user2, event_price: 199.99, event_name: "Evento de testes2", event_date: DateTime.now)
+Ticket.create!(user: user2, event_price: 229.99, event_name: "Evento de testes3", event_date: DateTime.now)
+Ticket.create!(user: user2, event_price: 339.99, event_name: "Evento de testes4", event_date: DateTime.now)
+Ticket.create!(user: user2, event_price: 459.99, event_name: "Evento de testes5", event_date: DateTime.now)
 
 
 
